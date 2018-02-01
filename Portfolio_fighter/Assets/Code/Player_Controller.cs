e20170685181134b79c15f22ba6598c4315fb548
Ochuko Ideh <ochuko97@hotmail.com>
2018-02-01 10:41:07 +0000

    Initial commit of unity folder

---
+++ Portfolio_fighter/Assets/Code/Player_Controller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour { 
+
+	public float speed; 
+
+	public Rigidbody2D playerrb; 
+
+	void FixedUpdate() { 
+		//Get move direction
+		float move = Input.GetAxis ("Horizontal"); 
+		//add velocity to the rigidbody in the move direction * our speed
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (move * speed, GetComponent<Rigidbody2D> ().velocity.y);
+	}
+}
